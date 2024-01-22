15c974d304b126c847f4fe8b9dd79920b08162bf
seheichenko-anna <seheichenko.anna@gmail.com>
2024-01-22 19:43:23 +0200

    Update

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,7 +1,7 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import iziToast from 'izitoast';
-import 'iziToast/dist/css/iziToast.min.css';
+import 'izitoast/dist/css/iziToast.min.css';
 
 let userSelectedDate;
 let countdownInterval;
